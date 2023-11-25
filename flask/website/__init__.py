10611a1e0ee83e751f2635177aed605891e9136b
jli293 <84085953+jli293@users.noreply.github.com>
2023-11-24 23:34:28 -0500

    commit - database configuration

--- flask/website/__init__.py
+++ flask/website/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from .routes import routes
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
-    #Creating new App
+    # Creating a new app
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "Simple Secret Key"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # Adding the database to the application
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     # Adding routes
-    from .routes import routes
     app.register_blueprint(routes, url_prefix="/")
-
     return app
