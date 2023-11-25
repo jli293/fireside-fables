32fa305e1cd718d617cd5870a975451a106683ce
jli293 <84085953+jli293@users.noreply.github.com>
2023-11-24 23:51:17 -0500

    commit - create a response model

---
+++ flask/website/models.py
@@ -0,0 +1,11 @@
+from . import db
+
+
+class Result(db.Model):
+    """
+    Create a Result table
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.String(20))
+    message_type = db.Column(db.String(100))
+    message = db.Column(db.String(500))
